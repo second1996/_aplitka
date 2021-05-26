a23295377336411d0d8e065d7e4abf6edecc998d
second1996 <vital10012@gmail.com>
2021-05-26 22:27:18 +0300

    git init

---
+++ app/js/bootstrap.bundle.js
@@ -0,0 +1,6 @@
+@@include('../../node_modules/popper.js/dist/umd/popper.min.js') // Popper lib
+@@include('../../node_modules/bootstrap/js/dist/util.js') // Bootstrap Util lib
+@@include('../../node_modules/bootstrap/js/dist/modal.js') // Bootstrap Modal
+@@include('../../node_modules/bootstrap/js/dist/dropdown.js') // Bootstrap Dropdown
+@@include('../../node_modules/bootstrap/js/dist/tab.js') // Bootstrap Tab
+@@include('../../node_modules/bootstrap/js/dist/collapse.js') // Bootstrap Collapse (Accordion)
---
+++ app/js/vendors.js
@@ -0,0 +1,4 @@
+@@include('../../node_modules/swiper/swiper-bundle.min.js') // Swiper slider
+@@include('../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js') // jQuery Mask
+@@include('../../node_modules/headhesive/dist/headhesive.min.js') // Headhesive (sticky header)
+@@include('../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js') // Fancybox gallery
