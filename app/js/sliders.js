7d1263282010f632145f2ebde9591c2a29581650
second1996 <vital10012@gmail.com>
2021-05-29 22:09:51 +0300

    Home page (sections 5 of 9). Code refactoring

--- app/js/sliders.js
+++ app/js/sliders.js
@@ -1,5 +1,45 @@
 $(document).ready(function() {
 
+	/**
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	* Home heroes banner slider
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	*/
+	new Swiper('.h-heroes-slider .swiper-container', {
+		slidesPerView: 1,
+		navigation: {
+			prevEl: '.h-heroes-slider-nav .swiper-button-prev',
+			nextEl: '.h-heroes-slider-nav .swiper-button-next',
+		},
+		pagination: {
+			el: '.h-heroes-slider-dots .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+	})
 
+
+	/**
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	* Home brands slider
+	*-------------------------------------------------------------------------------------------------------------------------------------------
+	*/
+	new Swiper('.h-brands-slider .swiper-container', {
+		slidesPerView: 6,
+		slidesPerGroup: 6,
+		spaceBetween: 16,
+		navigation: {
+			prevEl: '.h-brands-slider-nav .swiper-button-prev',
+			nextEl: '.h-brands-slider-nav .swiper-button-next',
+		},
+		pagination: {
+			el: '.h-brands-slider-dots .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
+	})
+
+
+	
 
 })
